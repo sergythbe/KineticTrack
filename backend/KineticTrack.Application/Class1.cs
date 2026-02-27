be512e99b15f29a35ddb978eec7bb61b6dbf488b
Sani Serge <sergyth@gmail.com>
2026-02-27 14:26:32 +0100

    création de la structure de base de mon backend

---
+++ backend/KineticTrack.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KineticTrack.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ backend/KineticTrack.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KineticTrack.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
