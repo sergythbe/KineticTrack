c1283428c5f02f2b76823430c0bf8e9eb0d6bb7c
Sani Serge <sergyth@gmail.com>
2026-05-28 10:44:10 +0200

    Feat: Finalisation de l'entité User.

--- KineticTrack.Domain/Entities/User.cs
+++ KineticTrack.Domain/Entities/User.cs
@@ -1,10 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace KineticTrack.Domain.Entities
 {
-    internal class User
+    public class User
     {
+        public Guid UserId { get; private set; }
+        public string PasswordHash { get; private set; }
+        public string Firstname { get; private set; }
+        public string Lastname { get; private set; }
+        public string Email { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsPasswordChanged { get; private set; }
+        public bool IsActive { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        //permet à EF de créér un USER
+        private User()
+        {
+            UserId = Guid.Empty;
+            PasswordHash = string.Empty;
+            Firstname = string.Empty;
+            Lastname = string.Empty;
+            Email = string.Empty;
+        }
+
+        public User(Guid userId, string passwordHash, string firstname, string lastname, string email)
+        {
+            UserId = userId;
+            PasswordHash = passwordHash;
+            Firstname = firstname.Trim();
+            Lastname = lastname.Trim();
+            Email = email.Trim().ToLower(); 
+            CreatedAt = DateTime.UtcNow;
+            IsPasswordChanged = false;
+            IsActive = true;
+            IsDeleted = false;
+        }
+
+      
+        public void UpdateProfile(string lastname, string firstname)
+        {
+            Lastname = lastname.Trim();
+            Firstname = firstname.Trim();
+        }
+
+        public void UpdateEmail(string email)
+        {
+            Email = email.Trim().ToLower();
+        }
+
+        public void DefineFirstPersonalPassword(string newPasswordHash)
+        {
+            PasswordHash = newPasswordHash;
+            IsPasswordChanged = true; 
+            IsActive = true;          
+        }
+
+        public void UpdatePassword(string passwordHash)
+        {
+            PasswordHash = passwordHash;
+        }
+
+        public void Deactivate() => IsActive = false;
+        public void Activate() => IsActive = true;
+
+        public void Delete()
+        {
+            IsDeleted = true;
+            IsActive = false;
+        }
     }
 }
