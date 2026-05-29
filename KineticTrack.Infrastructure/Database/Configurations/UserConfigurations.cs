74a46526572c935dcfd8648a684f822500a3c23d
serge thierry <sergythbe@gmail.com>
2026-05-29 07:58:07 +0200

    Feat/user-model:initialisation du DbContext et des configurations

---
+++ KineticTrack.Infrastructure/Database/Configurations/UserConfigurations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KineticTrack.Infrastructure.Database.Configurations
+{
+    internal class UserConfigurations
+    {
+    }
+}
---
+++ KineticTrack.Infrastructure/Repositories/UserRepositories.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KineticTrack.Infrastructure.Repositories
+{
+    internal class UserRepositories
+    {
+    }
+}
